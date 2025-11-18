c1b8c7b2fbfc8ca7255a9165f27f829a9926421e
dieferrui <dieferrui@alu.edu.gva.es>
2025-11-18 12:36:29 +0100

    plataforma + movimiento incercia

--- nuevo-proyecto-de-juego/Scripts/PersonajePrueba/personaje_prueba.gd
+++ nuevo-proyecto-de-juego/Scripts/PersonajePrueba/personaje_prueba.gd
@@ -1,69 +1,90 @@
 extends CharacterBody3D
 
-# Velocidad general del personaje
-@export var f_velocidad:float = 5.0
+# =======================
+# Configuración general
+# =======================
+@export var f_velocidad: float = 5.0
 @export var f_velocidad_caida: float = -9.8
+@export var f_velocidad_salto: float = 5.0
+@export var f_altura_maxima_salto: float = 10.0
+@export var f_friccion_suelo: float = 4.0
+@export var f_friccion_aire: float = 1.0
+@export var cam_camara: Camera3D
 
-# Parametros relacionados con el salto
-@export var f_velocidad_salto:float = 5.0
-@export var f_altura_maxima_salto:float = 10.0
-var b_saltando:bool = false
-var altura_inicio:float = 0.0
+# =======================
+# Variables internas
+# =======================
+var b_saltando: bool = false
+var altura_inicio: float = 0.0
 var altura_fin: float = 0.0
 
-@export var f_velocidad_rotacion:float = 1.0
-@export var cam_camara : Camera3D
-
-# Funcion para el salto
+# =======================
+# Funciones de salto
+# =======================
 func saltar() -> void:
 	if is_on_floor():
-		altura_inicio = global_position.y 
+		altura_inicio = global_position.y
 		altura_fin = altura_inicio + f_altura_maxima_salto
 		velocity.y = f_velocidad_salto
 		b_saltando = true
 
-# Funcion que determina la altura máxima que puede alcanzar un salto
 func limitar_salto() -> void:
-	if b_saltando: 
-		if velocity.y > 0 and global_position.y >= altura_fin:
-			velocity.y = 0
-			b_saltando = false
+	if b_saltando and velocity.y > 0 and global_position.y >= altura_fin:
+		velocity.y = 0
+		b_saltando = false
 
+# =======================
+# Física y movimiento
+# =======================
 func _physics_process(delta: float) -> void:
 	# Gravedad
 	if not is_on_floor():
 		velocity.y += f_velocidad_caida * delta
 
-	# Salta en caso de pulsar el boton asociado al salto
+	# Salto
 	if Input.is_action_just_pressed("saltar") and is_on_floor():
 		saltar()
-	
-	
+
 	limitar_salto()
-	
-	# Entrada del movimiento
-	var entrada_direccion := Vector2(
+
+	# Entrada de movimiento
+	var entrada_direccion = Vector2(
 		Input.get_action_strength("mover_derecha") - Input.get_action_strength("mover_izquierda"),
-		Input.get_action_strength("mover_arriba") - Input.get_action_strength("mover_abajo") 
+		Input.get_action_strength("mover_arriba") - Input.get_action_strength("mover_abajo")
 	)
-	
-	# Variable para la direccion
-	var direccion:Vector3 = Vector3.ZERO
-	
-	# Direccion relativa de la camara
-	var cam_delante := -cam_camara.transform.basis.z
-	var cam_derecha := cam_camara.transform.basis.x
-	
-	# Asigno la direccion que seguira el personaje a la direccion relativa de la camara
+
+	var direccion = Vector3.ZERO
+	var cam_delante = -cam_camara.transform.basis.z
+	var cam_derecha = cam_camara.transform.basis.x
 	direccion = (cam_delante * entrada_direccion.y) + (cam_derecha * entrada_direccion.x)
 	direccion.y = 0
 	direccion = direccion.normalized()
-	
-	if direccion:
-		velocity.x = direccion.x * f_velocidad
-		velocity.z = direccion.z * f_velocidad
-	else:
-		velocity.x = move_toward(velocity.x, 0, f_velocidad)
-		velocity.z = move_toward(velocity.z, 0, f_velocidad)
 
+	if direccion:
+		velocity.x += direccion.x * f_velocidad * delta
+		velocity.z += direccion.z * f_velocidad * delta
+	else:
+		if is_on_floor():
+			velocity.x = move_toward(velocity.x, 0, f_friccion_suelo * delta)
+			velocity.z = move_toward(velocity.z, 0, f_friccion_suelo * delta)
+		else:
+			velocity.x = move_toward(velocity.x, 0, f_friccion_aire * delta)
+			velocity.z = move_toward(velocity.z, 0, f_friccion_aire * delta)
+
+	# Mover el personaje primero
 	move_and_slide()
+
+	# Detectar trampolines y aplicar rebote
+	for i in range(get_slide_collision_count()):
+		var col = get_slide_collision(i)
+		var collider = col.get_collider()
+		var posible_trampolin = collider.get_parent()
+
+		if posible_trampolin is Trampolin:
+			aplicar_impulso_trampolin(posible_trampolin)
+
+# Función de impulso del trampolín
+func aplicar_impulso_trampolin(trampolin: Trampolin) -> void:
+	var normal = trampolin.get_normal().normalized()
+	var potencia = trampolin.get_potencia()
+	velocity += normal * potencia
--- nuevo-proyecto-de-juego/Scripts/Trampolin/trampolin.gd
+++ nuevo-proyecto-de-juego/Scripts/Trampolin/trampolin.gd
@@ -1,30 +1,12 @@
 class_name Trampolin
 extends Node3D
 
-@onready var rigidbody: RigidBody3D = $Body
-@export var fuerza_impulso: float = 15.0
-@export var direccion_superficie: Vector3 = Vector3.UP
+@export var fuerza_impulso: float = 50
 
 var tiempo_ultimo_impulso := {}
 
+func get_normal() -> Vector3:
+	return global_transform.basis.y
 
-func _ready():
-	rigidbody.contact_monitor = true
-	rigidbody.max_contacts_reported = 4
-
-func _integrate_forces(state):
-	var contact_count = state.get_contact_count()
-	if contact_count == 0:
-		return
-	
-	for i in range(contact_count):
-		var collider = state.get_contact_collider_object(i)
-		if collider is CharacterBody3D:
-			var now = Time.get_ticks_msec()
-			if tiempo_ultimo_impulso.get(collider, 0) + 200 > now:
-				continue
-			tiempo_ultimo_impulso[collider] = now
-			
-			var normal_superficie = (rigidbody.global_transform.basis * direccion_superficie).normalized()
-			
-			collider.launch(normal_superficie * fuerza_impulso)
+func get_potencia() -> float:
+	return fuerza_impulso
